ae1d0add80feddbfad69a4a00e8a7d34d028fc98
Hans-Georg Glöckler <h.g.gloeckler@gmx.de>
2019-03-05 11:14:14 +0100

    initial Commit

---
+++ src/main/java/org/pilger/PilgerWhoamiApplication.java
@@ -0,0 +1,13 @@
+package org.pilger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilgerWhoamiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PilgerWhoamiApplication.class, args);
+	}
+
+}
